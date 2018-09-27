b0af0f30d7aab0f1dab06b94305c24da5cc3b427
ReChinX <cyuunichin@gmail.com>
2018-09-27 16:39:47 +0800

    feat: engine update & support picacg(add unloaded file)

---
+++ app/src/main/java/top/rechinx/meow/engine/HttpDns.kt
@@ -0,0 +1,24 @@
+package top.rechinx.meow.engine
+
+import okhttp3.Dns
+import java.net.InetAddress
+import java.net.UnknownHostException
+import java.util.ArrayList
+
+class HttpDns(addresses: String) : Dns {
+
+    private var address = addresses
+
+    @Throws(UnknownHostException::class)
+    override fun lookup(paramString: String): List<InetAddress> {
+        try {
+            val list = ArrayList<InetAddress>()
+            list.add(InetAddress.getByName(address))
+            return list
+        } catch (localException: Exception) {
+            localException.printStackTrace()
+        }
+
+        return Dns.SYSTEM.lookup(paramString)
+    }
+}
