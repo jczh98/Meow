ba094e665dcc808da4bc24ef9a0929cc76f17f8e
ReChinX <cyuunichin@gmail.com>
2018-11-27 18:22:50 +0800

    fix: change filter style
    Signed-off-by: ReChinX <cyuunichin@gmail.com>

--- app/src/main/java/top/rechinx/meow/ui/filter/items/GroupItem.kt
+++ app/src/main/java/top/rechinx/meow/ui/filter/items/GroupItem.kt
@@ -34,9 +34,9 @@ class GroupItem(val filter: Filter.Group<*>) : AbstractExpandableHeaderItem<Grou
         holder.title.text = filter.name
 
         holder.icon.setImageResource(if (isExpanded)
-            R.drawable.ic_expand_more_white_24dp
+            R.drawable.ic_expand_more_black_24dp
         else
-            R.drawable.ic_chevron_right_white_24dp)
+            R.drawable.ic_chevron_right_black_24dp)
 
         holder.itemView.setOnClickListener(holder)
 
