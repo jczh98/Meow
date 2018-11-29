b4928860b96098ac1345d99891ce8d066ef768f3
ReChinX <cyuunichin@gmail.com>
2018-11-29 23:48:31 +0800

    feat: support remove history
    Signed-off-by: ReChinX <cyuunichin@gmail.com>

--- app/src/main/java/top/rechinx/meow/ui/grid/history/HistoryFragment.kt
+++ app/src/main/java/top/rechinx/meow/ui/grid/history/HistoryFragment.kt
@@ -1,10 +1,13 @@
 package top.rechinx.meow.ui.grid.history
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.view.ActionMode
+import com.google.android.material.snackbar.Snackbar
 import eu.davidea.flexibleadapter.FlexibleAdapter
+import eu.davidea.flexibleadapter.SelectableAdapter
+import eu.davidea.flexibleadapter.helpers.ActionModeHelper
 import kotlinx.android.synthetic.main.fragment_grid.*
 import top.rechinx.meow.R
 import top.rechinx.meow.data.database.model.Manga
@@ -17,9 +20,13 @@ import top.rechinx.rikka.mvp.factory.RequiresPresenter
 
 @RequiresPresenter(HistoryPresenter::class)
 class HistoryFragment: MvpFragment<HistoryPresenter>(),
-        FlexibleAdapter.OnItemClickListener {
+        FlexibleAdapter.OnItemClickListener,
+        FlexibleAdapter.OnItemLongClickListener,
+        ActionMode.Callback{
 
     private val adapter by lazy { GridAdapter(activity!!) }
+
+    private var actionModeHelper : ActionModeHelper? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_grid, container, false)
@@ -31,6 +38,9 @@ class HistoryFragment: MvpFragment<HistoryPresenter>(),
     }
 
     fun initViews() {
+        adapter.addListener(this)
+        actionModeHelper = ActionModeHelper(adapter, R.menu.menu_grid, this)
+                .withDefaultMode(SelectableAdapter.Mode.MULTI)
         adapter.addListener(this)
         recyclerView.adapter = adapter
     }
@@ -49,10 +59,56 @@ class HistoryFragment: MvpFragment<HistoryPresenter>(),
     }
 
     override fun onItemClick(view: View, position: Int) : Boolean{
-        val manga = adapter.getItem(position)?.manga ?: return false
-        val intent = DetailActivity.createIntent(activity!!, manga.sourceId, manga.url!!)
-        startActivity(intent)
+        if(adapter.mode != SelectableAdapter.Mode.IDLE && actionModeHelper != null) {
+            val active = actionModeHelper!!.onClick(position)
+            return active
+        } else {
+            val manga = adapter.getItem(position)?.manga ?: return false
+            val intent = DetailActivity.createIntent(activity!!, manga.sourceId, manga.url!!)
+            startActivity(intent)
+            return true
+        }
+    }
+
+    override fun onItemLongClick(position: Int) {
+        actionModeHelper?.onLongClick(activity as AppCompatActivity, position)
+    }
+
+    override fun onActionItemClicked(mode: ActionMode, item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.action_select_all -> {
+                adapter.selectAll()
+                return true
+            }
+            R.id.action_delete -> {
+                deleteSelectedItems()
+            }
+        }
+        return false
+    }
+
+    private fun deleteSelectedItems() {
+        presenter.deleteHistoryManga(adapter.selectedPositions.map {
+            val item = adapter.getItem(it)?.manga ?: return
+            return@map item.id
+        })
+    }
+
+    override fun onCreateActionMode(mode: ActionMode?, menu: Menu?): Boolean {
         return true
     }
 
+    override fun onPrepareActionMode(mode: ActionMode?, menu: Menu?): Boolean {
+        return false
+    }
+
+    override fun onDestroyActionMode(mode: ActionMode?) {
+    }
+
+    fun onHistoryDeleted(ids: List<Long>) {
+        for (id in ids) {
+            adapter.removeItemByMangaId(id)
+        }
+        Snackbar.make(layoutView, R.string.message_delete_success, Snackbar.LENGTH_SHORT)
+    }
 }
--- app/src/main/java/top/rechinx/meow/ui/grid/history/HistoryPresenter.kt
+++ app/src/main/java/top/rechinx/meow/ui/grid/history/HistoryPresenter.kt
@@ -1,13 +1,19 @@
 package top.rechinx.meow.ui.grid.history
 
+import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import org.koin.standalone.KoinComponent
 import org.koin.standalone.inject
+import top.rechinx.meow.data.database.dao.MangaDao
+import top.rechinx.meow.data.download.DownloadProvider
+import top.rechinx.meow.data.preference.getOrDefault
 import top.rechinx.meow.data.repository.MangaRepository
 import top.rechinx.rikka.mvp.BasePresenter
 
 class HistoryPresenter: BasePresenter<HistoryFragment>(), KoinComponent {
+
+    val mangaDao by inject<MangaDao>()
 
     private val mangaRepository: MangaRepository by inject()
 
@@ -19,6 +25,23 @@ class HistoryPresenter: BasePresenter<HistoryFragment>(), KoinComponent {
                 .subscribeLatestCache({ view, manga ->
                     view.onMangasLoaded(manga)
                 }, HistoryFragment::onMangasLoadError)
+    }
+
+    fun deleteHistoryManga(mangaIds: List<Long>) {
+        Observable.just(mangaIds)
+                .doOnNext {
+                    for (id in it) {
+                        val manga = mangaDao.load(id)
+                        if(manga != null) {
+                            manga.history = false
+                            mangaDao.updateManga(manga)
+                        }
+                    }
+                }.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeFirst({ view, ids ->
+                    view.onHistoryDeleted(ids)
+                })
     }
 
 }
