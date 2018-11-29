16e0348c2b1e742a628e7c99e2905c6b25be0636
ReChinX <cyuunichin@gmail.com>
2018-11-30 00:02:31 +0800

    feat: support long click to list preview
    Signed-off-by: ReChinX <cyuunichin@gmail.com>

--- app/src/main/java/top/rechinx/meow/ui/result/ResultActivity.kt
+++ app/src/main/java/top/rechinx/meow/ui/result/ResultActivity.kt
@@ -6,19 +6,26 @@ import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.recyclerview.widget.LinearLayoutManager
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.customview.customView
+import com.afollestad.materialdialogs.customview.getCustomView
 import com.scwang.smartrefresh.header.MaterialHeader
 import com.scwang.smartrefresh.layout.footer.ClassicsFooter
 import kotlinx.android.synthetic.main.activity_result.*
 import kotlinx.android.synthetic.main.custom_progress_bar.*
 import kotlinx.android.synthetic.main.custom_toolbar.*
+import kotlinx.android.synthetic.main.item_result.view.*
 import top.rechinx.meow.R
 import top.rechinx.meow.data.database.model.Manga
+import top.rechinx.meow.glide.GlideApp
 import top.rechinx.meow.global.Extras
 import top.rechinx.meow.ui.base.BaseAdapter
 import top.rechinx.meow.ui.details.DetailActivity
 import top.rechinx.rikka.mvp.MvpAppCompatActivityWithoutReflection
 
-class ResultActivity: MvpAppCompatActivityWithoutReflection<ResultPresenter>(), BaseAdapter.OnItemClickListener {
+class ResultActivity: MvpAppCompatActivityWithoutReflection<ResultPresenter>(), BaseAdapter.OnItemClickListener, BaseAdapter.OnItemLongClickListener {
 
     private val layoutManager = LinearLayoutManager(this)
     private val query: String by lazy { intent.getStringExtra(Extras.EXTRA_KEYWORD) }
@@ -45,6 +52,7 @@ class ResultActivity: MvpAppCompatActivityWithoutReflection<ResultPresenter>(), 
     private fun initViews() {
         resultAdapter = ResultAdapter(this, ArrayList())
         resultAdapter.setOnItemClickListener(this)
+        resultAdapter.setOnItemLongClickListener(this)
         recyclerView.layoutManager = layoutManager
         recyclerView.adapter = resultAdapter
         refreshLayout.setRefreshHeader(MaterialHeader(this))
@@ -88,6 +96,27 @@ class ResultActivity: MvpAppCompatActivityWithoutReflection<ResultPresenter>(), 
         val manga = resultAdapter.getItem(position)
         val intent = DetailActivity.createIntent(this, manga.sourceId, manga.url!!)
         startActivity(intent)
+    }
+
+    override fun onItemLongClick(view: View, position: Int) {
+        val manga = resultAdapter.getItem(position)
+        val dialog = MaterialDialog(this).show {
+            title(R.string.dialog_preview)
+            positiveButton {
+                it.dismiss()
+            }
+            customView(R.layout.custom_dialog_preivew)
+        }
+        val customView = dialog.getCustomView()
+        val title = customView?.findViewById<TextView>(R.id.title)
+        title?.text = manga.title
+        val cover = customView?.findViewById<ImageView>(R.id.cover)
+        cover?.let {
+            GlideApp.with(this)
+                .load(manga)
+                .into(it)
+        }
+        dialog.show()
     }
 
     companion object {
